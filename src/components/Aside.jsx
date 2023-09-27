468804bb25232a8179716c9ebd6b51403338a7e2
Teddy Pascual <teddylayson2000@gmail.com>
2023-09-27 15:17:14 +0800

    persistent aside bar

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -4,37 +4,39 @@ import { Avatar, AvatarGroup } from "@mui/material";
 import Images from "./Images";
 const Aside = ({ online }) => {
   return (
-    <div className=" border-2 border-solid flex flex-col items-center border-gray-200 gap-5 p-4">
-      <MdTitle text={"Online Friends"} />
-      <AvatarGroup max={6} total={online}>
-        <Avatar
-          alt="Teddy Pascual"
-          src="./src/assets/profile.jpg"
-          className="w-15 h-15"
-        />
-        <Avatar
-          alt="Teddy Pascual"
-          src="./src/assets/profile.jpg"
-          className="w-15 h-15"
-        />
-        <Avatar
-          alt="Teddy Pascual"
-          src="./src/assets/profile.jpg"
-          className="w-15 h-15"
-        />
-        <Avatar
-          alt="Teddy Pascual"
-          src="./src/assets/profile.jpg"
-          className="w-15 h-15"
-        />
-        <Avatar
-          alt="Teddy Pascual"
-          src="./src/assets/profile.jpg"
-          className="w-15 h-15"
-        />
-      </AvatarGroup>
-      <MdTitle text={"Latest Photos"} />
-      <Images />
+    <div className=" border-2 border-solid border-gray-200">
+      <div className="fixed p-8 gap-5 flex flex-col items-center">
+        <MdTitle text={"Online Friends"} />
+        <AvatarGroup max={6} total={online}>
+          <Avatar
+            alt="Teddy Pascual"
+            src="./src/assets/profile.jpg"
+            className="w-15 h-15"
+          />
+          <Avatar
+            alt="Teddy Pascual"
+            src="./src/assets/profile.jpg"
+            className="w-15 h-15"
+          />
+          <Avatar
+            alt="Teddy Pascual"
+            src="./src/assets/profile.jpg"
+            className="w-15 h-15"
+          />
+          <Avatar
+            alt="Teddy Pascual"
+            src="./src/assets/profile.jpg"
+            className="w-15 h-15"
+          />
+          <Avatar
+            alt="Teddy Pascual"
+            src="./src/assets/profile.jpg"
+            className="w-15 h-15"
+          />
+        </AvatarGroup>
+        <MdTitle text={"Latest Photos"} />
+        <Images />
+      </div>
     </div>
   );
 };
