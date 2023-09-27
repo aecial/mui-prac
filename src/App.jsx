440e996b761d9de15320116906c555360138c152
Teddy Pascual <teddylayson2000@gmail.com>
2023-09-28 07:29:53 +0800

    added functionality to sideNav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { CssBaseline } from "@mui/material";
 import Navbar from "./components/Navbar";
 import SideNavigation from "./components/SideNavigation";
 import Aside from "./components/Aside";
 import Home from "./components/Home";
-
+import Pages from "./components/Pages";
+import Groups from "./components/Groups";
 function App() {
+  const [mainCont, setMainCont] = useState(0);
+  const contents = [<Home />, <Pages />, <Groups />];
+
   return (
     <>
       {/* <PhotoAlbum /> */}
@@ -13,8 +17,12 @@ function App() {
       <Navbar />
       <main className="p-4 items-center gap-5">
         <div className="grid grid-cols-1 md:grid-cols-3">
-          <SideNavigation />
-          <Home />
+          <SideNavigation
+            onClick={() => setMainCont(1)}
+            homeClick={() => setMainCont(0)}
+            groupClick={() => setMainCont(2)}
+          />
+          {contents[mainCont]}
           <Aside online={10} />
         </div>
       </main>
--- src/components/SideNavigation.jsx
+++ src/components/SideNavigation.jsx
@@ -7,23 +7,23 @@ import {
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import AutoStoriesOutlinedIcon from "@mui/icons-material/AutoStoriesOutlined";
 import GroupsOutlinedIcon from "@mui/icons-material/GroupsOutlined";
-const SideNavigation = () => {
+const SideNavigation = ({ homeClick, onClick, groupClick }) => {
   return (
     <div className="bg-slate-100 uppercase hidden md:block">
       <List className="p-4 flex flex-col gap-4 fixed w-96">
-        <ListItemButton className="shadow-sm shadow-black">
+        <ListItemButton className="shadow-sm shadow-black" onClick={homeClick}>
           <ListItemIcon>
             <HomeOutlinedIcon fontSize="large" />
           </ListItemIcon>
           <ListItemText primary="Home" />
         </ListItemButton>
-        <ListItemButton className="shadow-sm shadow-black">
+        <ListItemButton className="shadow-sm shadow-black" onClick={onClick}>
           <ListItemIcon>
             <AutoStoriesOutlinedIcon fontSize="large" />
           </ListItemIcon>
           <ListItemText primary="Pages" />
         </ListItemButton>
-        <ListItemButton className="shadow-sm shadow-black">
+        <ListItemButton className="shadow-sm shadow-black" onClick={groupClick}>
           <ListItemIcon>
             <GroupsOutlinedIcon fontSize="large" />
           </ListItemIcon>
